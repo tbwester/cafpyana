9a871c1fc9bfb64e948a4edff01a02a660b860f3
Thomas Wester <thomas_w@live.com>
2026-05-04 15:02:00 +0000

    pid scores from other planes

--- analysis_village/kaon/makedf/make_kaon_df.py
+++ analysis_village/kaon/makedf/make_kaon_df.py
@@ -195,7 +195,12 @@ def make_kaon_recodf(f: pd.DataFrame, save_track_truth: bool=False) -> pd.DataFr
     pandora_df = ph.multicol_merge(pandora_df, daughter_id, left_index=True, right_index=True, how='left')
 
     # kaon pid score
-    kpid = ph.loadbranches(f["recTree"], ['rec.slc.reco.pfp.trk.chi2pid.2.chi2_kaon']).rec.slc.reco
+    kpid = ph.loadbranches(f["recTree"], [
+        'rec.slc.reco.pfp.trk.chi2pid.0.chi2_kaon',
+        'rec.slc.reco.pfp.trk.chi2pid.1.chi2_kaon',
+        'rec.slc.reco.pfp.trk.chi2pid.2.chi2_kaon',
+        'rec.slc.reco.pfp.trk.bestplane'
+    ]).rec.slc.reco
     pandora_df = ph.multicol_merge(pandora_df, kpid, left_index=True, right_index=True, how='left')
 
     # barycenter flash match
